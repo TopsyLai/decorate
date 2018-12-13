99f7690ced979ac5c913031190b4d97f5d81e18e
TopsyLai <243312648@qq.com>
2018-12-13 18:16:08 +0800

    1812131817

--- src/views/personal/stationed.vue
+++ src/views/personal/stationed.vue
@@ -8,19 +8,41 @@
       </flexbox-item>
       <flexbox-item>年</flexbox-item>
     </flexbox>
-    <cell title="角色" value="请选择您的角色定位"></cell>
-    <cell-form-preview :list="role"></cell-form-preview>
+
+      <flexbox>
+        <flexbox-item>
+          <cell title="角色" @click.native="roleBoxShow=true" :value="roleName"></cell>
+
+        </flexbox-item>
+    </flexbox>
+    <flexbox>
+      <flexbox-item :span="7/12" >
+        <x-input title='期望金额' placeholder="请输入金额" type="tel"></x-input>
+      </flexbox-item>
+      <flexbox-item>
+        <popup-picker :data="unitList" v-model="unit" placeholder="期望金额单位">
+        </popup-picker>
+      </flexbox-item>
+    </flexbox>
+    <flexbox>
+      <flexbox-item>
+        <x-textarea title="自我描述" placeholder="简介 简单描述自己的特长及其他擅长的技能" :show-counter="false" :height="200" :rows="8" :cols="30"></x-textarea>
+      </flexbox-item>
+    </flexbox>
+    <flexbox>
+      <flexbox-item>
+        <popup-picker :data="addressData" title="地址" @on-shadow-change="changeAddressData"  v-model="regionId"></popup-picker>
+      </flexbox-item>
+    </flexbox>
+    <!-- <cell-form-preview :list="role"></cell-form-preview> -->
     <!-- <popup-radio multiple title="角色" :options="roleList" v-model="role" placeholder="请选择您的角色定位"></popup-radio> -->
     </group>
 
     <div v-transfer-dom>
-      <popup v-model="show13" position="bottom" max-height="50%">
-        <group>
-          <cell v-for="i in 20" :key="i" :title="i"></cell>
+      <popup v-model="roleBoxShow" position="bottom" max-height="50%">
+        <group gutter="0">
+          <checklist label-position="left" :options="roleList" @on-change="change" v-model="role" ></checklist>
         </group>
-        <div style="padding: 15px;">
-          <x-button @click.native="show13 = false" plain type="primary"> Close Me </x-button>
-        </div>
       </popup>
     </div>
 
@@ -40,7 +62,11 @@ import {
   Flexbox,
   FlexboxItem,
   Popup,
-  CellFormPreview 
+  CellFormPreview,
+  Checklist,
+  XInput,
+  XTextarea,
+  XAddress
 } from "vux";
 
 export default {
@@ -56,7 +82,11 @@ export default {
     Flexbox,
     FlexboxItem,
     Popup,
-    CellFormPreview 
+    CellFormPreview,
+    Checklist,
+    XInput,
+    XTextarea,
+    XAddress
   },
   directives: {
     TransferDom
@@ -68,16 +98,20 @@ export default {
       tabIndex: 0,
       userInfo: {},
       portraitShow: false,
+      roleBoxShow: false,
       workAge: [],
-      workAgeList: [[0,1,2,3,4,5,6,7,8,9,10]],
+      workAgeList: [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
       role: [],
-      roleList: ['电工','木工','油漆工'],
-      expectAmount: '',
-      unit: '',
-      shiningPoint: '',
+      roleName: [],
+      roleList: ["电工", "木工", "油漆工"],
+      unit: [],
+      unitList: [["元/平米","元/小时","元/天","元/月"]],
+      expectAmount: "",
+      shiningPoint: "",
+      addressData: [],
       UserTypeList: {},
       UserRoleList: [],
-      regionId: '',
+      regionId: ['','',''],
       portrait: {
         camera: '<label = for="file">拍照</label>',
         avatar: '<label = for="file">从相册中选择</label>'
@@ -90,26 +124,96 @@ export default {
       self.UserTypeList = response.data;
     });
     this.$fetch("common/getUserRoleList.htm").then(response => {
-      self.roleList = response.data;
+      let roleList = response.data;
+      var arr = [];
+      for (let k in roleList) {
+        let data = {
+          'value': roleList[k],
+          'key': k
+        };
+        arr.push(data);
+      }
+      self.roleList = arr;
     });
-    
+    this.$fetch("common/getPayUnitList.htm").then(response => {
+      self.unitList = [response.data];
+    });
+      this.$fetch("common/queryRegionView.htm").then(response => {
+        for (let k =0 ;k<response.data.length;k++) {
+          response.data[k].value = response.data[k].id
+        }
+        self.addressData = [response.data];
+        for (let k =0 ;k<response.data[0].citys.length;k++) {
+          response.data[0].citys[k].value = response.data[0].citys[k].id
+        }
+        self.addressData[1] = response.data[0].citys;
+        for (let k =0 ;k<response.data[0].citys[0].countys.length;k++) {
+          response.data[0].citys[0].countys[k].value = response.data[0].citys[0].countys[k].id
+        }
+        self.addressData[2] = response.data[0].citys[0].countys;
+        // self.queryCityListByProvince(response.data[0])
+      });
   },
   methods: {
     changeTabIndex(index) {
       this.tabIndex = index;
     },
+    queryCountyListByCity(city,county){
+      for (let k =0 ;k<self.addressData[city].citys[county].countys.length;k++) {
+        self.addressData[0][city].citys[county].countys[k].value = self.addressData[0][city].citys[county].countys[k].id
+      }
+      self.addressData[2] = self.addressData[0][city].citys[county].countys;
+    },
+    changeAddressData(item,name,xxx){
+      let self = this;
+      for (let k =0 ;k<self.addressData[0].length;k++) {
+        if(self.addressData[0][k].id==item[0]){
+          if(self.addressData[1][0].id==self.addressData[0][k].citys[0].id){
+            for (let j =0 ;k<self.addressData[1].citys.length;k++) {
+              debugger
+              if(self.addressData[1].citys[k].id==item[1]){
+                //self.queryCountyListByCity(item[1],0)
+              }
+            }
+          }else{
+            self.queryCityListByProvince(k,0)
+          }
+        }
+      }
+    },
+    queryCityListByProvince(city,county){
+      let self = this;
+      if(self.addressData[1][0].id==self.addressData[0][city].citys[0].id){
+        return false;
+      }else{
+        for (let k =0 ;k<self.addressData[0][city].citys.length;k++) {
+          self.addressData[0][city].citys[k].value = self.addressData[0][city].citys[k].id
+        }
+        self.$set(self.addressData,1,self.addressData[0][city].citys);
+      }
+      if(county==0){
+        self.queryCountyListByCity(city,county)
+      }
+    },
+    change(value, label){
+      if(label.length==0){
+        this.roleName = '请选择您的角色定位';
+      }else{
+        this.roleName = label.toString();
+      }
+    },
     skill() {
       let self = this;
       let params = new FormData();
-      params.append("workAge", self.workAge);//工人工作年限 【0~10】下拉选
-      params.append("role", self.role);//用户角色 做成下拉选 或者其他 此字段现在不确定 可能要做成多选
-      params.append("expectAmount", self.expectAmount);//期望金额
-      params.append("unit", self.unit);//期望金额单位 P("元/平米"),	X("元/小时"), T("元/天"), Y("元/月");
-      params.append("shiningPoint", self.shiningPoint);//简介 简单描述自己的特长及其他擅长的技能
-      params.append("regionId", self.regionId);//期望工作地id shiningPoint 
+      params.append("workAge", self.workAge); //工人工作年限 【0~10】下拉选
+      params.append("role", self.role); //用户角色 做成下拉选 或者其他 此字段现在不确定 可能要做成多选
+      params.append("expectAmount", self.expectAmount); //期望金额
+      params.append("unit", self.unit); //期望金额单位 P("元/平米"),	X("元/小时"), T("元/天"), Y("元/月");
+      params.append("shiningPoint", self.shiningPoint); //简介 简单描述自己的特长及其他擅长的技能
+      params.append("regionId", self.regionId); //期望工作地id shiningPoint
       self.$post("skill/addSkill.htm", params).then(
         function(json) {
-          self.$vux.toast.text(json.data)
+          self.$vux.toast.text(json.data);
           self.queryUserInfo();
         },
         function(json) {
